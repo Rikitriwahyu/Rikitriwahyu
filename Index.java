5ea9973d339017190980470e4a4ba3afd7c43c86
Rikitriwahyu <93129402+Rikitriwahyu@users.noreply.github.com>
2021-10-25 18:18:25 +0700

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,10 @@
+
+import components.*;
+class Index{
+    public static void main(String[] args){
+        var a  = new ViewData();
+        a.line();
+        a.ViewHeader("Dimsum Dapoer Wangi");
+        a.line();
+    }
+}
---
+++ ViewData.java
@@ -0,0 +1,10 @@
+package components;
+
+ public class ViewData {
+    public void ViewHeader(String dataTitan) {
+        System.out.println("========== "+dataTitan+" =======");
+    }
+    public void line() {
+        System.out.println("=======================================");
+    }
+}
